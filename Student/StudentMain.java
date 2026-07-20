e5dc603268fc753be20017703e4e2e6af6a94aff
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-20 14:08:06 +0530

    Implement main method in StudentMain class
    Added main method to demonstrate Student class functionality.

---
+++ Student/StudentMain.java
@@ -0,0 +1,24 @@
+package com.constructor;
+
+public class StudentMain {
+
+	
+	public static void main(String[] args) {
+		 
+		Student s1 = new Student("Ram", 70);
+		s1.displayResult();
+		Student s2 = new Student("Rahul", 45);
+		s2.displayResult();
+	}
+
+}
+
+---------------------------------------------------------------------------
+  OUTPUT : 
+Name: Ram
+Marks: 70
+Result: Pass
+-------------------------------------------------------
+Name: Rahul
+Marks: 45
+Result: Pass
