6fbc28d22a3ef9d06e475f79d9927c4e1436e495
Vanshika Ramoshi <vanshikaramoshi664@gmail.com>
2026-07-20 14:56:49 +0530

    Implement main method in EmployeeMain class
    Added main method to demonstrate Employee salary calculation.

---
+++ Employee/EmployeeMain.java
@@ -0,0 +1,18 @@
+package com.constructor;
+
+public class EmployeeMain {
+
+	public static void main(String[] args) {
+		
+		Employee e1 = new Employee("Vanshika" , 50000);
+		e1.calculatesalary();
+	}
+
+}
+
+----------------------------------------------------------------------
+  OUTPUT :
+Employee Name : Vanshika
+Basic salary : 50000.0
+Bonus 20% : 10000.0
+Total Salary : 60000.0
